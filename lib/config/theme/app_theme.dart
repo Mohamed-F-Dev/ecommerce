431979859e617c2a3e18c8c9dec920864b3a55e5
Nuha Samy <nuha23397samy@gmail.com>
2026-02-03 04:51:47 +0200

    logintwo

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -3,8 +3,19 @@ import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   static final ThemeData light = ThemeData(
-    textTheme: GoogleFonts.poppinsTextTheme().copyWith(),
     brightness: Brightness.light,
+    textTheme: GoogleFonts.poppinsTextTheme().copyWith(),
+    colorScheme: const ColorScheme.light(
+      primary: Color(0xff2D201C),
+      onPrimary: Color(0xffFFFFFF),
+    ),
   );
-  static final ThemeData dark = ThemeData(brightness: Brightness.dark);
+  static final ThemeData dark = ThemeData(
+    brightness: Brightness.dark,
+    textTheme: GoogleFonts.poppinsTextTheme(),
+    colorScheme: const ColorScheme.dark(
+      primary: Color(0xffFFFFFF),
+      onPrimary: Color(0xff2D201C),
+    ),
+  );
 }
